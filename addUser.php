343ed5b36db7006eb82c83509585fbf8ee0a0d22
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2020-02-13 23:29:56 -0600

    dimos formato

--- addUser.php
+++ addUser.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add User</title>
+    <link rel="stylesheet" href="styles.css">
+
 </head>
 <body>
     
 
-    <input type="text">Nombre: 
+    <div class="etiquetas">
+        Nombre: <input type="text">
+    </div>
 
 </body>
 </html>
